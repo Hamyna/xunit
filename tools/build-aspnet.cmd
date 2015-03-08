8cbd3ab6aea6662509613be4d7dff84f434a950c
N. Taylor Mullen <nimullen@microsoft.com>
2015-03-08 13:01:52 -0700

    Update kvm/KVM/Kvm => dnvm/DNVM/Dnvm.

--- tools/build-aspnet.cmd
+++ tools/build-aspnet.cmd
@@ -1,13 +1,13 @@
-@echo off
+﻿@echo off
 pushd %~dp0\..
 
 rem Have to bust the cache, because of broken packages from the CLR team
 rmdir /q /s %USERPROFILE%\.kpm\packages > nul 2>&1
 
 rem Make sure beta 2 is installed and in use
-powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\kvm.ps1 install 1.0.0-beta2 -runtime CLR -x86
-powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\kvm.ps1 install 1.0.0-beta2 -runtime CoreCLR -x86
-powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\kvm.ps1 use 1.0.0-beta2 -runtime CLR -x86
+powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\dnvm.ps1 install 1.0.0-beta2 -runtime CLR -x86
+powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\dnvm.ps1 install 1.0.0-beta2 -runtime CoreCLR -x86
+powershell -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned tools\dnvm.ps1 use 1.0.0-beta2 -runtime CLR -x86
 
 rem Restore packages and build
 call kpm restore
